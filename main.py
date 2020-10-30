23ea3a725a3dec5fad50d6047bed1ff2c20acc08
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 18:33:04 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def exponential(lam) :
+  # Your function to generate an exponential random variable goes here
+  
+def firstevent(lam1,lam2) : 
+  # Your function to return if Alice and Bob meet at the bus stop goes here
+  
+def samplemean(n,lam1,lam2) :
+  # Your function to calculate the sample mean from first event goes here
+  
+  
+# The 20 samples you take of the random variable should go in the list called samples  
+samples=20*[0]
+# Your code to generate the 20 resamples of the mean goes here
+
+
+# These variables need to be set equal to the 25th percentile, the median and the 75th percentile
+# of the sample in samples respectively in order to complete the exercise 
+percentile25 =
+median =
+percentile75 =
+print( percentile25, median, percentile75 )
+
+  
